68612c28faeded5c61812ba42a768500bbd15785
wenxiaofang <858874032@qq.com>
2019-04-16 20:35:12 +0800

    Updates

---
+++ precache-manifest.b96c793be9e68cbd679227456b053718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/archi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ba7cacec0d09d325d26c",
+    "url": "/archi/static/js/runtime~main.e1b54785.js"
+  },
+  {
+    "revision": "a8ad755e10d0098190ef",
+    "url": "/archi/static/js/main.b2cbad74.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/archi/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a8ad755e10d0098190ef",
+    "url": "/archi/static/css/main.3968fb2e.chunk.css"
+  },
+  {
+    "revision": "ae2f8bb050a5dcea84a96a7057b37c84",
+    "url": "/archi/index.html"
+  }
+];
